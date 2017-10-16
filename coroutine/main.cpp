e4ff7982b3b4964639cd006daf36bb82a1e867dc
jruan <379171482@qq.com>
2017-10-16 21:47:35 +0800

    测试

--- coroutine/main.cpp
+++ coroutine/main.cpp
@@ -22,29 +22,29 @@ void f()
 	coroutine::init(1024 * 1024);
 	char nDummy;
 	printf("thread 0x%x\n", (uintptr_t)&nDummy);
-	uint64_t nCoroutineID1 = coroutine::create(10 * 4096, std::bind(&fun1, std::placeholders::_1));
+	//uint64_t nCoroutineID1 = coroutine::create(10 * 4096, std::bind(&fun1, std::placeholders::_1));
 	uint64_t nCoroutineID2 = coroutine::create(0, std::bind(&fun2, std::placeholders::_1));
 	printf("CCCCCC222\n");
-	coroutine::resume(nCoroutineID1, 0);
+	//coroutine::resume(nCoroutineID1, 0);
 	coroutine::resume(nCoroutineID2, 0);
 	coroutine::resume(nCoroutineID2, 0);
 	printf("DDDDD222\n");
-	coroutine::resume(nCoroutineID1, 0);
-	uint64_t nCoroutineID3 = coroutine::create(1024*1024, std::bind(&fun1, std::placeholders::_1));
-	printf("CCCCCC333\n");
-	coroutine::resume(nCoroutineID3, 0);
-	coroutine::resume(nCoroutineID3, 0);
+	//coroutine::resume(nCoroutineID1, 0);
+// 	uint64_t nCoroutineID3 = coroutine::create(1024*1024, std::bind(&fun1, std::placeholders::_1));
+// 	printf("CCCCCC333\n");
+// 	coroutine::resume(nCoroutineID3, 0);
+// 	coroutine::resume(nCoroutineID3, 0);
 	printf("EEEEEE333\n");
 }
 
 int main()
 {
 	std::thread t0(f);
- 	std::thread t1(f);
- 	std::thread t2(f);
+//  	std::thread t1(f);
+//  	std::thread t2(f);
 	t0.join();
- 	t1.join();
- 	t2.join();
+//  	t1.join();
+//  	t2.join();
 
 #ifdef _WIN32
 	system("pause");
